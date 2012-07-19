07c3e9cecdf49d31ea6e3666c3d92b9e1729c9e8
Dmitry Kislyuk <kislyuk.d@gmail.com>
2012-07-19 03:32:56 -0700

    v2

--- app.py
+++ app.py
@@ -3,12 +3,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from pymongo import Connection
+
+connection = Connection('mongodb://admin:sync2200@staff.mongohq.com:10068/synced')
+db = connection['synced']
+config = db['config'].find_one()
+idx_count = config['idx_count']
+tracks = db['mixes']
+
+
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return 'Hello World!' + str(idx_count)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     # port = int(os.environ.get('PORT', 5027))
-    port = 5027
+    port = 5000
     app.run(host='0.0.0.0', port=port)
